5ee0be6800d39a5ae93cad2281d8538bb2207065
Dean Attali <daattali@gmail.com>
2014-11-14 19:31:34 -0800

    add tolowerfirst function

---
+++ R/tolowerfirst.R
@@ -0,0 +1,8 @@
+tolowerfirst <- function(x) {
+	stopifnot(is.character(x))
+	return(paste0(tolower(substring(x, 1, 1)), substring(x, 2)))
+}
+
+tolowerfirst("CamelCase")
+tolowerfirst("ALLCAPS")
+tolowerfirst(c("First", "_Second"))
