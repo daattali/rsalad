8263bbb7fa0c93d744d8c928a80cf9c4f7645dba
Dean Attali <daattali@gmail.com>
2015-04-09 14:25:19 -0700

    fix broken tests (looks like testthat doesn't see whole package, just the test files)

--- tests/testthat/test-rutilsMock.R
+++ tests/testthat/test-rutilsMock.R
@@ -1,32 +1,32 @@
 context("rutilsMock")
 
-# turn off the tests for now becuase I can't figure out a nice way to ensure
-# the directory paths will be correct both when doing a check and when testing
-# using devtools::test()
-
-if (FALSE) {
-
 test_that("isDirectoryMock is TRUE when passed a directory", {
-  expect_true(isDirectoryMock("../../R"))
-  expect_true(isDirectoryMock("../../R/"))
+  expect_true(isDirectoryMock("."))
+  expect_true(isDirectoryMock("../testthat"))
   expect_true(isDirectoryMock("../../tests"))
+  expect_true(isDirectoryMock("../../tests/"))
 })
 
 test_that("isDirectoryMock is FALSE when passed a file or non-existent dir", {
-  expect_false(isDirectoryMock("../../DESCRIPTION"))
-  expect_false(isDirectoryMock("../../NAMESPACE"))
-  expect_false(isDirectoryMock("../../lalanodir"))
+  expect_false(isDirectoryMock("nosuchdir"))
+  expect_false(isDirectoryMock("nosuchdir/"))
+  expect_false(isDirectoryMock("test-rutilsMock.R"))
+  expect_false(isDirectoryMock("./test-rutilsMock.R"))
+  expect_false(isDirectoryMock("../testthat.R"))
 })
 
 test_that("isFileMock is TRUE when passed a file", {
-  expect_true(isFileMock("../../DESCRIPTION"))
-  expect_true(isFileMock("../../NAMESPACE"))
+  expect_true(isFileMock("test-rutilsMock.R"))
+  expect_true(isFileMock("./test-rutilsMock.R"))
+  expect_true(isFileMock("../testthat.R"))
 })
 
 test_that("isFileMock is FALSE when passed a directory or non-existent file", {
-  expect_false(isFileMock("../../R"))
-  expect_false(isFileMock("../../R/"))
-  expect_false(isFileMock("../../lalanofile"))
+  expect_false(isFileMock("."))
+  expect_false(isFileMock("../testthat"))
+  expect_false(isFileMock("../../tests"))
+  expect_false(isFileMock("../../tests/"))
+  expect_false(isFileMock("test-nosuchfile.R"))
 })
 
 test_that("isAbsolutePathMock is TRUE when passed an absolute path (including Windows/unix style or ~)", {
@@ -46,5 +46,3 @@ test_that("isAbsolutePathMock is FALSE when passed a relative path", {
   expect_false(isAbsolutePathMock("\\"))
   expect_false(isAbsolutePathMock("CD:"))
 })
-
-}
